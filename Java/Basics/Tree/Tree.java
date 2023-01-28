51b9c76e04f14bd70234b8b76ca13810fe191b01
Sebastian Bustos <86377478+tatancode@users.noreply.github.com>
2023-01-28 10:28:07 -0600

    Practice programs of the week 1-28-23

---
+++ Java/Basics/Tree/Tree.java
@@ -0,0 +1,31 @@
+// Define your tree class in this file!
+/** 
+* Introductions and Tree
+*/
+public class Tree {
+
+  public static void main(String[] args){
+
+    // We are going to intruduce ourselves
+    System.out.println("Hey there, I'm Sebastian and I'm learning to code in Java!");
+
+    // We are going to plant a tree
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+    System.out.println("######");
+    System.out.println();
+    System.out.println();
+    System.out.println("* ** *");
+    System.out.println("******");
+    System.out.println(" ****");
+    System.out.println("  **");
+    System.out.println("  **");
+    System.out.println();
+    System.out.println("~~~~~~~~");
+    
+  }
+}
