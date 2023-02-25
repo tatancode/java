a83b9ae62eb84615cf5ba435f4af92ade8bc321d
Sebastian Bustos <86377478+tatancode@users.noreply.github.com>
2023-02-25 12:25:01 -0600

    Java Basics 2/17/2023

---
+++ Java/Basics/Continents/Continents.java
@@ -0,0 +1,25 @@
+/*
+ * Program prints out a continent and the largest city
+ * in that continent, based on the value of an integer
+ */
+public class Continents {
+
+    public static void main(String[] args) {
+        int continent = 4;
+
+        switch(continent){
+            case 1:
+                System.out.println("North America: Mexico City, Mexico");
+                break;
+            case 2:
+                System.out.println("South America: Sao Paulo, Brazil");
+                break;
+            case 3:
+                System.out.println("Europe: Moscow, Russia");
+                break;
+            case 4:
+                System.out.println("Africa: Lagos, Nigeria");
+                break;
+        }
+    }
+}
